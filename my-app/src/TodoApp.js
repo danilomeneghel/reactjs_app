65497747679c2ce90c2a414d9c1b36205c886252
danilo.meneghel <danilo.meneghel@gmail.com>
2019-04-18 03:46:11 -0300

    Update files

--- my-app/src/TodoApp.js
+++ my-app/src/TodoApp.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {NotificationContainer, NotificationManager} from 'react-notifications';
 
 class TodoApp extends React.Component {
   constructor(props) {
@@ -6,26 +7,51 @@ class TodoApp extends React.Component {
     this.state = { items: [], text: '' };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+	this.handleDelete = this.handleDelete.bind(this);
   }
+   
+  createNotification = (type) => {
+      switch (type) {
+        case 'remove':
+          NotificationManager.info('Item removido.', 'Excluído!');
+          break;
+        case 'success':
+          NotificationManager.success('Item adicionado.', 'Sucesso!');
+          break;
+        case 'warning':
+          NotificationManager.warning('Preencha os dados corretamente.', 'Atenção!', 3000);
+          break;
+        case 'error':
+          NotificationManager.error('Verifique os dados digitados.', 'Erro!', 5000, () => {
+            alert('callback');
+          });
+          break;
+		default:
+		  alert('Erro!');
+      }
+  };
 
   render() {
     return (
       <div>
-        <h3>TODO</h3>
-        <TodoList items={this.state.items} />
+	    <NotificationContainer/>
         <form onSubmit={this.handleSubmit}>
+		  <br />
           <label htmlFor="new-todo">
-            What needs to be done?
-          </label><br />
+            Digite o item a ser adicionado
+          </label>
+		  <br />
           <input
             id="new-todo"
             onChange={this.handleChange}
             value={this.state.text}
           />
-          <button>
-            Add #{this.state.items.length + 1}
-          </button>
+          <button className='btn btn-success'>
+		    Adicionar
+		  </button>
         </form>
+		
+		<TodoList items={this.state.items} handleDelete={this.handleDelete} />
       </div>
     );
   }
@@ -37,28 +63,42 @@ class TodoApp extends React.Component {
   handleSubmit(e) {
     e.preventDefault();
     if (!this.state.text.length) {
-      return;
-    }
-    const newItem = {
-      text: this.state.text,
-      id: Date.now()
-    };
-    this.setState(prevState => ({
-      items: prevState.items.concat(newItem),
-      text: ''
-    }));
+		this.createNotification('warning');
+		return;
+    } else {
+		const newItem = {
+		  text: this.state.text,
+		  id: Date.now()
+		};
+		this.setState(prevState => ({
+		  items: prevState.items.concat(newItem),
+		  text: ''
+		}));
+		this.createNotification('success');
+	}
+  }
+     
+  handleDelete(id){
+	this.setState(prevState => ({
+		items: prevState.items.filter(el => el !== id )
+	}));
+	this.createNotification('remove');
   }
 }
 
 class TodoList extends React.Component {
+  handleDelete(id){
+    this.props.handleDelete(id);
+  }
+
   render() {
-    return (
-      <ul>
-        {this.props.items.map(item => (
-          <li key={item.id}>{item.text}</li>
-        ))}
-      </ul>
-    );
+	return (
+		<ol>
+			{this.props.items.map(item => (
+				<li key={item.id}>{item.text} <button onClick={this.handleDelete.bind(this, item)}>Excluir</button></li>
+			))}
+		</ol>
+	);
   }
 }
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import TodoApp from './TodoApp';
 import registerServiceWorker from './registerServiceWorker';
 
+import 'react-notifications/lib/notifications.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<TodoApp />, document.getElementById('myapp'));
 registerServiceWorker();
