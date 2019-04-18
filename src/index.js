6ae7a7cf7ef439290aedcb62c369bd74f2c3195c
danilo.meneghel <danilo.meneghel@gmail.com>
2019-04-18 18:34:30 -0300

    Update files

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import TodoApp from './TodoApp';
 import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
-import '../node_modules/font-awesome/css/font-awesome.min.css';
-import '../node_modules/react-notifications/lib/notifications.css';
+import 'react-notifications/lib/notifications.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<TodoApp />, document.getElementById('myapp'));
